f72a8af4a2174b7144264a0817fceddbb31dab76
fktona <adetona.fk@gmail.com>
2023-09-29 10:50:55 +0100

    uuid

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
+const { uuid } = require('uuid');
 const serviceAccount = require('./secret.json')
 require('dotenv').config()
 
